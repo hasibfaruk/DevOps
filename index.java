fa6b6a4bd8f912033b351ce0ab97ec128038709f
21BTRSE065 <164789648+hasibfaruk@users.noreply.github.com>
2024-03-25 06:08:32 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,13 @@
+public class Challenge {
+	public static String textToNum(String phone) {
+		phone=phone.replaceAll("[ABC]", "2")
+							 .replaceAll("[DEF]", "3")
+							 .replaceAll("[GHI]", "4")
+							 .replaceAll("[JKL]", "5")
+							 .replaceAll("[MNO]", "6")
+							 .replaceAll("[PQRS]","7")
+							 .replaceAll("[TUV]","8")
+							 .replaceAll("[WXYZ]","9");
+		return phone;
+	}
+}
